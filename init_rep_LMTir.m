b564daf73a5b37c3a746d86691d94eb9963aa75b
Luc <l2@crans.org>
2014-02-04 17:41:09 +0100

    Modifs de la procédure d'ajout des chemins dans le PATH pour permettre l'ajout direct des autres Toolbox

--- init_rep_LMTir.m
+++ init_rep_LMTir.m
@@ -6,8 +6,17 @@ function init_rep_LMTir(chemin)
 %dossier de la Toolbox LMTir
 doss={'routines','tirages','tirages/IHS','tirages/LHS'};
 
-%si pas de chemin spécifié
+%en fonction des parametres
+specif_chemin=true;
 if nargin==0
+    specif_chemin=false;
+elseif nargin>1
+    if isempty(chemin)
+        specif_chemin=false;
+    end
+end
+%si pas de chemin specifie
+if ~specif_chemin
     chemin=pwd;
 end
 
@@ -16,3 +25,17 @@ chemin_full=cellfun(@(c)[chemin '/' c],doss,'uni',false);
 
 %ajout au PATH
 cellfun(@addpath,chemin_full);
+
+if nargin==2
+    %%chargement des autres toolbox
+    if ~iscell(other);other={other};end
+    %chemins absolus
+    chemin_full=cellfun(@(c)[chemin '/../' c],other,'uni',false);
+    %ajout au PATH
+    cellfun(@addpath,chemin_full);
+    
+    %ajout des toolbox dans le PATH
+    nom_fct=cellfun(@(c)['init_rep_' c],other,'uni',false);
+    cellfun(@feval,nom_fct,chemin_full)
+end
+end
