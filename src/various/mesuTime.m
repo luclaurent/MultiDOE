9d168b0210133c801bded8765aff53ffb0352966
Luc <l2@crans.org>
2016-08-31 11:18:00 +0200

    Improve the class for measuring time

--- src/various/mesuTime.m
+++ src/various/mesuTime.m
@@ -15,7 +15,7 @@
 %     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 %
 
-%% Script dedicated to the initiailisation, the measure and the display of the spent time
+%% Script dedicated to the initialisation, the measure and the display of the spent time
 %% L.LAURENT -- 15/05/2012 -- luc.laurent@lecnam.net
 
 classdef mesuTime < handle
@@ -40,17 +40,29 @@ classdef mesuTime < handle
             obj.tInit=cputime;
         end
         %stop the measure
-        function stop(obj)
+        function stop(obj,flagDisp)
+            if nargin==1;flagDisp=true;end
             %Measure initial CPU time
             obj.tElapsed=toc(obj.counterTicToc);
             %Elapsed time
             obj.tCPUElapsed=cputime-obj.tInit;
             %show the result
-            show(obj);
+            if flagDisp
+                show(obj);
+            end
+        end
+        %get CPU time elasped
+        function timeElapsed=cpuElapsed(obj)
+            if isempty(obj.tCPUElapsed);obj.stop(false);end
+            timeElapsed=obj.tCPUElapsed;
+        end
+        %get time elasped
+        function timeElapsed=Elapsed(obj)
+            if isempty(obj.tCPUElapsed);obj.stop(false);end
+            timeElapsed=obj.tElapsed;
         end
         %display the elapsed time
         function show(obj)
-            fprintf(' - - - - - - - - - - - - - - - - - - - - \n')
             fprintf('  #### Time/CPU time (s): %4.2f s / %4.2f s\n',obj.tElapsed,obj.tCPUElapsed);
         end
     end
